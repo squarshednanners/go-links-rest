c731d06675ec9cf03d655c1150bd25dc0105a607
Brad Bass <brad.bass@gmail.com>
2018-01-29 10:50:14 -0600

    Adding a few info log statements for summary deletion

--- src/main/java/com/go/bac/impl/UsageSummaryBACImpl.java
+++ src/main/java/com/go/bac/impl/UsageSummaryBACImpl.java
@@ -1,5 +1,6 @@
 package com.go.bac.impl;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -53,7 +54,9 @@ public class UsageSummaryBACImpl implements IUsageSummaryBAC {
 	public void calculateHourlySummary() {
 		Long hourStart = hourSummaryBegin();
 		Long hourEnd = hourSummaryEnd();
+		LOGGER.info("Calculating Hourly Summary from " + new Date(hourStart) + " to " + new Date(hourEnd));
 		List<UsageLog> hourlyUsageLogs = usageLogBAR.fetchUsageLogsByInterval(hourStart, hourEnd, SortDirection.DESC);
+		LOGGER.debug(hourlyUsageLogs.size() + " records found for hourly summary");
 		UsageSummary summary = createSummary(hourStart, hourlyUsageLogs);
 		usageSummaryBAR.addHourlySummary(summary);
 	}
@@ -62,7 +65,9 @@ public class UsageSummaryBACImpl implements IUsageSummaryBAC {
 	public void calculateDailySummary() {
 		Long dayStart = daySummaryBegin();
 		Long dayEnd = daySummaryEnd();
+		LOGGER.info("Calculating Daily Summary from " + new Date(dayStart) + " to " + new Date(dayEnd));
 		List<UsageLog> dailyUsageLogs = usageLogBAR.fetchUsageLogsByInterval(dayStart, dayEnd, SortDirection.DESC);
+		LOGGER.debug(dailyUsageLogs.size() + " records found for daily summary");
 		UsageSummary summary = createSummary(dayStart, dailyUsageLogs);
 		usageSummaryBAR.addDailySummary(summary);
 		aggregateTotalSummary();
@@ -71,6 +76,7 @@ public class UsageSummaryBACImpl implements IUsageSummaryBAC {
 	private void deleteHourlyUsageSummary(Long deleteThreshold) {
 		boolean hasMoreLogs = true;
 		int pageNum = 1;
+		int summariesDeleted = 0;
 		while (hasMoreLogs) {
 			List<UsageSummary> tempUsageSummary = usageSummaryBAR.fetchPagedHourlySummary(pageNum++, 25);
 			if (tempUsageSummary.isEmpty()) {
@@ -80,17 +86,20 @@ public class UsageSummaryBACImpl implements IUsageSummaryBAC {
 				if (usageSummary.getStartTime() < deleteThreshold) {
 					LOGGER.debug("Deleting Hourly Usage Summary: " + usageSummary.toString());
 					usageSummaryBAR.deleteHourlySummary(usageSummary);
+					summariesDeleted++;
 				} else {
 					hasMoreLogs = false;
 					break;
 				}
 			}
 		}
+		LOGGER.info("Deleted " + summariesDeleted + " Usage Summaries");
 	}
 
 	private void deleteDailyUsageSummary(Long deleteThreshold) {
 		boolean hasMoreLogs = true;
 		int pageNum = 1;
+		int summariesDeleted = 0;
 		while (hasMoreLogs) {
 			List<UsageSummary> tempUsageSummary = usageSummaryBAR.fetchPagedDailySummary(pageNum++, 2);
 			if (tempUsageSummary.isEmpty()) {
@@ -100,12 +109,14 @@ public class UsageSummaryBACImpl implements IUsageSummaryBAC {
 				if (usageSummary.getStartTime() < deleteThreshold) {
 					LOGGER.debug("Deleting Daily Usage Summary: " + usageSummary.toString());
 					usageSummaryBAR.deleteDailySummary(usageSummary);
+					summariesDeleted++;
 				} else {
 					hasMoreLogs = false;
 					break;
 				}
 			}
 		}
+		LOGGER.info("Deleted " + summariesDeleted + " Usage Summaries");
 	}
 
 	private UsageSummary createSummary(Long startTime, List<UsageLog> usageLogs) {
